634b466643c7e017d77246d645c4b3bc18d2140d
Sefinek <contact@sefinek.net>
2026-09-06 19:07:03 +0200

    rate-limit-redis & cleanup

--- middlewares/ratelimit.js
+++ middlewares/ratelimit.js
@@ -1,11 +1,16 @@
 const rateLimit = require('express-rate-limit');
+const { RedisStore } = require('rate-limit-redis');
+const RedisClient = require('../services/redis.js');
 const RenderError = require('../utils/renderError.js');
+
+const redisStore = prefix => new RedisStore({ sendCommand: (...args) => RedisClient.sendCommand(args), prefix });
 
 exports.global = rateLimit({
 	windowMs: 60 * 1000,
 	limit: 100,
 	standardHeaders: 'draft-7',
 	legacyHeaders: false,
+	store: redisStore('mcpl:ratelimit:global:'),
 	handler: (req, res) => RenderError(res, 429),
 });
 
@@ -14,6 +19,7 @@ exports.contactForm = rateLimit({
 	limit: 3,
 	standardHeaders: 'draft-7',
 	legacyHeaders: false,
+	store: redisStore('mcpl:ratelimit:contactForm:'),
 	skip: req => req.method !== 'POST',
 	handler: (req, res) => {
 		if (req.get('accept') === 'application/json') return res.status(429).json({ errors: ['Zbyt wiele żądań. Spróbuj ponownie za chwilę.'] });
